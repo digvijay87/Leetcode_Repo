7d6a31daea2f33173006b2ee40663c801fdf28cd
digvijay87 <digvijay.chauhan87@gmail.com>
2022-04-14 16:52:02 +0530

    Create 743__Network_Delay_Time.cpp

---
+++ Graph/743__Network_Delay_Time.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        vector<pair<int,int>> g[N+1];
+        
+        for(int i = 0; i < times.size(); i++)
+        {
+            g[times[i][0]].push_back(make_pair(times[i][1],times[i][2]));
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+        
+        q.push(make_pair(0,K));
+        
+        vector<int> dist(N+1, INT_MAX);
+        dist[K] = 0;
+        pair<int,int> temp;
+        
+        bool visit[N+1];
+        memset(visit, false, sizeof(visit));
+        
+        while(!q.empty())
+        {
+            temp = q.top();
+            q.pop();
+            
+            int u = temp.second;
+            visit[u] = true;
+            
+            for(int i = 0; i < g[u].size(); i++)
+            {
+                int v = g[u][i].first;
+                int weight = g[u][i].second;
+                
+                if(visit[v] == false && dist[v] > dist[u] + weight)
+                {
+                    dist[v] = dist[u] + weight;
+                    q.push(make_pair(dist[v],v));
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i=1;i<dist.size();i++){
+            ans = max(ans, dist[i]);
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;
+        
+        
+        
+    }
+};
